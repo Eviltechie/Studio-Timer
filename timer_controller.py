cb8c63d40da32a32e0e6ca7393a83b22af809930
Ivan Panarusky <panaruskyi@gmail.com>
2024-03-30 18:16:52 -0500

    very close

--- timer_controller.py
+++ timer_controller.py
@@ -7,11 +7,21 @@ class TimerController:
         self.timers = [timer.Timer(self), timer.Timer(self), timer.Timer(self)]
         self.active_timer = self.timers[0]
     
+    def time_seconds_to_string(self):
+        time_seconds = self.active_timer.time
+        
+        hours = time_seconds // 3600
+        remainder = time_seconds % 3600
+        
+        minutes = remainder // 60
+        seconds = remainder % 60
+        
+        self.time_string = "{:02d}{:02d}{:02d}".format(hours, minutes, seconds)
+    
     def notify(self, timer):
         if timer == self.active_timer:
-            print("notify active")
-        else:
-            print("notify")
+            self.time_seconds_to_string();
+            print(self.time_string)
 
 x = TimerController()
 x.timers[0].set_time(300)
