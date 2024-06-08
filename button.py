faa3b28a8852119b692e0f2fbb65eb5bc82d2a78
Ivan Panarusky <panaruskyi@gmail.com>
2024-06-07 22:37:35 -0500

    can't get stuck in reverse state now

--- button.py
+++ button.py
@@ -14,21 +14,20 @@ class Button:
         self.pin = machine.Pin(pin_number, machine.Pin.IN, machine.Pin.PULL_UP)
         self.pin.irq(handler=self.handle_irq, trigger=machine.Pin.IRQ_FALLING | machine.Pin.IRQ_RISING, hard=True)
         self.callback_ref = self.callback
-        self.pressed = False
     
     # Handle the hard interupt
     def handle_irq(self, pin):
         self.pin.irq(handler=None)
-        micropython.schedule(self.callback_ref, None)
+        micropython.schedule(self.callback_ref, pin.value())
     
     # Re-enable the hard interupt
     def reset_irq(self):
         self.pin.irq(handler=self.handle_irq, trigger=machine.Pin.IRQ_FALLING | machine.Pin.IRQ_RISING, hard=True)
     
     # The interrupt handler schedules this to run so we can allocate memory and such
-    def callback(self, x):
-        self.pressed = not self.pressed
-        if self.pressed and self.on_press is not None:
+    def callback(self, value):
+        # value 0 is pressed, 1 is released
+        if not value and self.on_press is not None:
             self.on_press()
         elif self.on_release is not None:
             self.on_release()
